2ef43ab26d1be1dbd33db07e36d021ec8cf479cd
Gabriel Musse <gdmusse@hotmail.com>
2021-03-23 15:59:54 -0300

    Adicionei Labenusers

---
+++ semana7/labenusers/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import AddUsers from "./components/AddUsers";
+import GetAllUsers from "./components/GetAllUsers";
+
+export default class App extends React.Component {
+  state = {
+    usersCreated: false,
+  };
+
+  createPage = () => {
+    this.setState({ usersCreated: true });
+  };
+
+  listPage = () => {
+    this.setState({ usersCreated: false });
+  };
+
+  render() {
+    const renderWindow = () => {
+      if (this.state.usersCreated) {
+        return <GetAllUsers changepage={this.listPage} />;
+      } else {
+        return <AddUsers changepage={this.createPage} />;
+      }
+    };
+    return <div className="App">{renderWindow()}</div>;
+  }
+}
